55fb504f3220d35df699d17aa34b8ade877eda83
Roman Agapitov <agapitov820@gmail.com>
2023-01-18 23:46:41 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 100;
+        int replenishment = 1000;
+
+        int bonus;
+
+        if (replenishment <= 1000){bonus = 0;}
+        else {bonus = replenishment / 100;}
+        System.out.println("Ваши бонусы составляют: " + bonus + " рублей");
+
+        if (replenishment > 1000){balance = replenishment + balance + bonus;}
+        else {balance = (balance + replenishment);}
+        System.out.println("Ваш баланс составляет: " + balance + " рублей");
+    }
+}
